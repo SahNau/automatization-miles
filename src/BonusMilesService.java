bb82d5185d212267cd69828854d2aeb1a06a76e9
Alexander Naumov <aleksnn00@gmail.com>
2021-03-16 15:59:39 +0500

    add 2

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,5 +1,8 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        return cost / 20;
+        int part = 20;
+        int miles  = cost / part;
+        return miles;
     }
+
 }
